0c13e2572908fdb425adf3203bf62f728fe55ebc
Stanley Huang <infamous_godhand@yahoo.com>
2020-06-02 19:57:39 +1000

    added more content to basic pages

--- pages/base.js
+++ pages/base.js
@@ -1,1 +1,32 @@
-export default () => <h1>Base</h1>;
+import React from "react";
+import Link from "next/link";
+
+const Base = ({ addBase, pizza }) => {
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
+
+  return (
+    <div className="base container">
+      <h3>Step 1: Choose Your Base</h3>
+      <ul>
+        {bases.map((base) => {
+          let spanClass = pizza.base === base ? "active" : "";
+          return (
+            <li key={base} onClick={() => addBase(base)}>
+              <span className={spanClass}>{base}</span>
+            </li>
+          );
+        })}
+      </ul>
+
+      {pizza.base && (
+        <div className="next">
+          <Link to="/toppings">
+            <button>Next</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Base;
--- pages/order.js
+++ pages/order.js
@@ -1,1 +1,15 @@
-export default () => <h1>order</h1>;
+import React from "react";
+
+const Order = ({ pizza }) => {
+  return (
+    <div className="container order">
+      <h2>Thank you for your order :)</h2>
+      <p>You ordered a {pizza.base} pizza with:</p>
+      {pizza.toppings.map((topping) => (
+        <div key={topping}>{topping}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Order;
