bd4456d9859ddac3b64d023c033b2493c90fb0ed
Stanley Huang <infamous_godhand@yahoo.com>
2020-06-03 19:47:49 +1000

    showing stuff on base and order page

--- pages/base.js
+++ pages/base.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Link from "next/link";
+import { connect } from "react-redux";
 import Layout from "../components/Layout";
 
-const Base = () => {
+const Base = ({ pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
@@ -10,26 +11,28 @@ const Base = () => {
       <div className="base container">
         <h3>Step 1: Choose Your Base</h3>
         <ul>
-          {/*bases.map((base) => {
+          {bases.map((base) => {
             let spanClass = pizza.base === base ? "active" : "";
             return (
-              <li key={base} onClick={() => addBase(base)}>
+              <li key={base}>
                 <span className={spanClass}>{base}</span>
               </li>
             );
-          })*/}
+          })}
         </ul>
 
-        {/*pizza.base && (
+        {pizza.base && (
           <div className="next">
             <Link href="/toppings">
               <button>Next</button>
             </Link>
           </div>
-        )*/}
+        )}
       </div>
     </Layout>
   );
 };
-
-export default Base;
+const mapState = (state) => ({
+  pizza: state.pizza,
+});
+export default connect(mapState)(Base);
--- pages/order.js
+++ pages/order.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 import Layout from "../components/Layout";
 
-const Order = () => {
+const Order = ({ pizza }) => {
   return (
     <Layout>
       <div className="container order">
         <h2>Thank you for your order :)</h2>
         <p>You ordered a pizza pizza with:</p>
-        {/*{pizza.toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
-      ))}*/}
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
       </div>
     </Layout>
   );
 };
-
-export default Order;
+const mapState = (state) => ({
+  pizza: state.pizza,
+});
+export default connect(mapState)(Order);
